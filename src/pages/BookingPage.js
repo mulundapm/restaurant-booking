86f4684a4185bc7e75a573ac027961627e96e1b4
mulundapm <mulundapm@yahoo.com.hk>
2024-05-27 22:53:15 +0200

    share data across pages

--- src/pages/BookingPage.js
+++ src/pages/BookingPage.js
@@ -1,8 +1,7 @@
-import React from 'react'
+import React, { createContext , useState} from 'react'
 import MainLayout from '../Layout/MainLayout'
 import restaurantImage from '../asset/row-of-serviced-tables-row-in-a-cafe-terrace-2023-11-27-05-24-53-utc.jpeg'
-import { Link } from "react-router-dom";
-import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function BookingPage() {
 
@@ -14,92 +13,93 @@ function BookingPage() {
     name: ""
   })
 
-
-
   const handleChange = (e) => {
   const name = e.target.name;
   const value = e.target.value;
   setInputs(values => ({...values, [name]: value}))
 }
 
+const navigate = useNavigate();
+
   const handleSubmit=(e) =>{
     e.preventDefault();
     console.log(JSON.stringify(inputs));
+    navigate('/confirmation', {state: inputs});
   }
-
 
   return (
     <MainLayout>
-      <div className='booking'>
-          <h1 className='green'>Reserve a table</h1>
-          <img src={restaurantImage} alt="Restaurant" />
-          <h6>39 Kingsroad, Chicago</h6>
-          <form onSubmit={handleSubmit}>
-            <div className="row">
-              <label htmlFor="date">Date</label>
-              <input
-                type="date"
-                name="date"
-                id="date"
-                value={inputs.date||""}
-                onChange={handleChange}/>
-            </div>
-            <div className="row">
-              <label htmlFor="time">Time</label>
-              <select
-                name="time"
-                value={inputs.time}
-                id="time"
-                onChange={handleChange}
-                autoComplete="off">
-                <option value="" disabled hidden>Select</option>
-                <option value="1900">19:00</option>
-                <option value="1930">19:30</option>
-                <option value="2000">20:00</option>
-                <option value="2030">20:30</option>
-                <option value="2100">21:00</option>
-                <option value="2130">21:30</option>
-              </select>
-            </div>
-            <div className="row">
-              <label htmlFor="visitors">Visitors</label>
-              <input
-                type="number"
-                name="visitors"
-                id="visitors"
-                value={inputs.visitors}
-                onChange={handleChange}
-                autoComplete="off"/>
-            </div>
-            <div className="row">
-              <label name="occasion">Occasion</label>
-              <select
-                name="occasion"
-                value={inputs.occasion}
-                id="occasion"
-                onChange={handleChange}
-                autoComplete="off">
-                <option value="">Select</option>
-                <option value="birthday">Birthday</option>
-                <option value="anniversary">Anniversary</option>
-              </select>
-            </div>
-            <div className="row">
-              <label htmlFor="name">Name</label>
-              <input
-                type="text"
-                name="name"
-                value={inputs.name}
-                id="name"
-                onChange={handleChange}
-                autoComplete="off"/>
-            </div>
-            <button type='submit' className='primaryButton' onSubmit={handleSubmit}>Reserve a table</button>
-          </form>
-      </div>
+        <div className='booking'>
+            <h1 className='green'>Reserve a table</h1>
+            <img src={restaurantImage} alt="Restaurant" />
+            <h6>39 Kingsroad, Chicago</h6>
+            <form onSubmit={handleSubmit}>
+              <div className="row">
+                <label htmlFor="date">Date</label>
+                <input
+                  type="date"
+                  name="date"
+                  id="date"
+                  value={inputs.date||""}
+                  onChange={handleChange}/>
+              </div>
+              <div className="row">
+                <label htmlFor="time">Time</label>
+                <select
+                  name="time"
+                  value={inputs.time}
+                  id="time"
+                  onChange={handleChange}
+                  autoComplete="off">
+                  <option value="" disabled hidden>Select</option>
+                  <option value="19:00">19:00</option>
+                  <option value="19:30">19:30</option>
+                  <option value="20:00">20:00</option>
+                  <option value="20:30">20:30</option>
+                  <option value="21:00">21:00</option>
+                  <option value="21:30">21:30</option>
+                </select>
+              </div>
+              <div className="row">
+                <label htmlFor="visitors">Visitors</label>
+                <input
+                  type="number"
+                  name="visitors"
+                  id="visitors"
+                  value={inputs.visitors}
+                  onChange={handleChange}
+                  autoComplete="off"/>
+              </div>
+              <div className="row">
+                <label name="occasion">Occasion</label>
+                <select
+                  name="occasion"
+                  value={inputs.occasion}
+                  id="occasion"
+                  onChange={handleChange}
+                  autoComplete="off">
+                  <option value="">Select</option>
+                  <option value="birthday">Birthday</option>
+                  <option value="anniversary">Anniversary</option>
+                </select>
+              </div>
+              <div className="row">
+                <label htmlFor="name">Name</label>
+                <input
+                  type="text"
+                  name="name"
+                  value={inputs.name}
+                  id="name"
+                  onChange={handleChange}
+                  autoComplete="off"/>
+              </div>
+              <button type='submit' className='primaryButton' > Reserve a table</button>
+            </form>
+        </div>
     </MainLayout>
 
   )
 }
 
 export default BookingPage
+export const InputContext = createContext({})
--- src/pages/ConfirmationPage.js
+++ src/pages/ConfirmationPage.js
@@ -1,31 +1,33 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import MainLayout from '../Layout/MainLayout'
 import check from '../asset/Solid check.svg'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 function ConfirmationPage() {
+  const location = useLocation();
+  const inputs = location.state;
   return (
     <MainLayout>
       <div className="confirmation">
         <h1 className='green'>Confirmation</h1>
         <img src={check} alt="check icon" />
-        <h5>Table reserved for userName</h5>
+        <h5>Table reserved for {inputs.name}</h5>
         <div className="res-info">
           <div className="row">
             <h6>Date:</h6>
-            <p id='date'>03/12/2024</p>
+            <p id='date'>{inputs.date}</p>
           </div>
           <div className="row">
             <h6>Time:</h6>
-            <p id='time'>20:00</p>
+            <p id='time'>{inputs.time}</p>
           </div>
           <div className="row">
             <h6>Visitors:</h6>
-            <p id='visitor'>8</p>
+            <p id='visitor'>{inputs.visitors}</p>
           </div>
           <div className="row">
             <h6>Occasion:</h6>
-            <p id='occasion'>Birthday</p>
+            <p id='occasion'>{inputs.occasion}</p>
           </div>
         </div>
         <button className='primaryButton'><Link to="/">Back to homepage</Link></button>
