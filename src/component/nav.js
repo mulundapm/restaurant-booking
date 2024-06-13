97cbad760c79527323e87021ad56de2ab005500d
mulundapm <mulundapm@yahoo.com.hk>
2024-06-13 23:16:11 +0200

    add hamburger menu

--- src/component/nav.js
+++ src/component/nav.js
@@ -3,12 +3,20 @@ import mainLogo from "../asset/Asset 16@4x.png"
 import { BsCartPlus } from "react-icons/bs";
 import './nav.css'
 import { Link } from "react-router-dom";
+import { IoMdClose } from "react-icons/io";
 
 function Nav(){
+    const closePopUpMenu = () => {
+        
+    }
+
+    const showPopUpMenu = () => {
+        console.log()
+    }
     return(
         <>
         <div className="nav">
-            <GiHamburgerMenu className="react-icons hamburger nav-item" size={40}/>
+            <GiHamburgerMenu className="react-icons hamburger nav-item" size={40} onClick={showPopUpMenu}/>
             <Link to="/"><img
                 className="nav-item"
                 src={mainLogo}
@@ -16,7 +24,15 @@ function Nav(){
                 width={150}
             /></Link>
             <BsCartPlus className="react-icons cart nav-item" size={40}/>
-
+            <div className="hamburgerMenu">
+            <button className="closeButton secondaryButton" onClick={closePopUpMenu}><IoMdClose />Close</button>
+                <img src={mainLogo} alt="Logo" width={150}/>
+                <h5>Order</h5>
+                <h5>Reservation</h5>
+                <h5>Reviews</h5>
+                <h5>About</h5>
+                <h5>Contact Us</h5>
+            </div>
         </div>
         </>
     )}
